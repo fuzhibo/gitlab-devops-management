03bfefca9141b736322190781955c7d0fe605f7b
ZhiBo Fu <fuzhibo@tom.com>
2019-12-20 18:05:15 +0800

    Update extension.

--- devops/src/extension.ts
+++ devops/src/extension.ts
@@ -8,20 +8,23 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devops" is now active!');
+	console.log('GitLab devops management start...');
+	let gitlabUrl = vscode.workspace.getConfiguration('gitlabDevopsMgt').instanceUrl;
+	let gitlabToken = vscode.workspace.getConfiguration('gitlabDevopsMgt').personalAccessToken;
+	console.log('GitLab URL: ' + gitlabUrl + ' GitLab Token:' + gitlabToken);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('gldevops.precheck', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Begin to do precheck.');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
